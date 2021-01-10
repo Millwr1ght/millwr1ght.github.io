a9b6c523f80ad2db91a52bb95d512b6d5323cfd5
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-01-09 20:42:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var d = new Date();
-document.getElementById("thisYear").innerHTML = d.getFullYear();
-document.getElementById("lastUpdated").innerHTML = document.lastModified;
+let d = new Date();
+document.getElementById("thisYear").innerHTML = toString(d.getFullYear());
+console.log(d, d.getFullYear());
+document.getElementById("lastUpdated").innerHTML = toString(document.lastModified);
