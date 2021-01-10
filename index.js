78eba18bcb74d6b0b5ef475dd7f055b1b7df5bc3
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-01-09 21:01:23 -0800

    aityahbdv sbm
    fdsgadfhgfxvcxbfdhgsdbvcfdgsfvzvbcxdfgsfgzbvcbdfs vcbdfvc xxvcbzdgzbvc xvxbgdsgzbvc xcvbdf

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let d = new Date();
 console.log(d, d.getFullYear());
 document.getElementById('thisYear').textContent = d.getFullYear();
 
-let lastModified = new Date();
+let lastModified = new Date(document.lastModified);
 document.getElementById("lastUpdated").innerHTML = lastModified.toUTCString();
