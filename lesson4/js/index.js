718a14e24eb415e1823ae4cbcb1faa306c35d3be
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-01-30 19:43:32 -0800

    akljghhdfghbjk
    dvfbdnfhewrbewrte yjr

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,10 +1,10 @@
-let d = new Date();
-console.log(d, d.getFullYear());
-document.getElementById('thisYear').textContent = d.getFullYear();
-
 let lastModified = document.lastModified;
 console.log(document.lastModified, lastModified);
 document.getElementById('lastUpdated').innerHTML = lastModified;
+
+let d = new Date();
+console.log(d.getFullYear());
+document.getElementById('thisYear').textContent = d.getFullYear();
 
 function navToggle() {
     document.getElementById('top-nav').classList.toggle("hidden");
