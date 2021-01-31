8e301894a8079f72c074de757d2f11973af5c3e7
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-01-30 18:39:18 -0800

    probably should reference things properly
    like say, the assignment instructions

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -5,3 +5,14 @@ document.getElementById('thisYear').textContent = d.getFullYear();
 let lastModified = document.lastModified;
 console.log(document.lastModified, lastModified);
 document.getElementById('lastUpdated').innerHTML = lastModified;
+
+var width = window.innerWidth
+
+function navToggle() {
+    let topNav = document.getElementById('top-nav');
+    if (topNav.className === "top-nav") {
+        topNav.className += " responsive";
+    } else {
+        topNav.className = "top-nav";
+    }
+}
