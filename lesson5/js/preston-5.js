77558e449a72dfa5068f956615ea82832411c8e3
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-05 17:42:56 -0800

    Update preston-5.js

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -4,10 +4,17 @@ console.log(lastModified, updateSpan);
 updateSpan.innerHTML = lastModified;
 
 let d = new Date();
+let today = d.getDay();
 let yearSpan = document.getElementById('thisYear');
 console.log(d.getFullYear());
-yearSpan.textContent = d.getFullYear();
+yearSpan.textContent = d.getFullYear()
 
 function navToggle() {
     document.getElementById('top-nav').classList.toggle("hidden");
 }
+
+function isTodayFriday (today) {
+    return (today == 5) ? document.getElementById('friday-deal').classList.toggle("hidden"): console.log(today, 'not friday');
+}
+
+isTodayFriday(today);
