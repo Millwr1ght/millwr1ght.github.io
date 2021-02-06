4f851b15af89a9ca611cd448471ad6315620a6d7
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-05 16:59:26 -0800

    all assets in same folder

---
+++ lesson5/js/preston-5.js
@@ -0,0 +1,13 @@
+let lastModified = document.lastModified;
+let updateSpan = document.getElementById('lastUpdated');
+console.log(lastModified, updateSpan);
+updateSpan.innerHTML = lastModified;
+
+let d = new Date();
+let yearSpan = document.getElementById('thisYear');
+console.log(d.getFullYear());
+yearSpan.textContent = d.getFullYear();
+
+function navToggle() {
+    document.getElementById('top-nav').classList.toggle("hidden");
+}
