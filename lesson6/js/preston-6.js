279d42e2004fe2efd35dede1a6f8aa80893813bd
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-11 21:19:03 -0800

    dfdgfhdgngbmn
    sgfndg n

---
+++ lesson6/js/preston-6.js
@@ -0,0 +1,25 @@
+let lastModified = document.lastModified;
+const updateSpan = document.getElementById('lastUpdated');
+console.log(lastModified, updateSpan);
+updateSpan.innerHTML = lastModified;
+
+let d = new Date();
+let today = d.getDay();
+const yearSpan = document.getElementById('thisYear');
+console.log(d.getFullYear());
+yearSpan.textContent = d.getFullYear()
+
+function navToggle() {
+    document.getElementById('top-nav').classList.toggle("responsive");
+}
+
+function isTodayFriday (today) {
+    return (today == 5 ? document.getElementById('friday-deal').classList.toggle("hidden"): console.log(today, 'not friday'));
+}
+
+function redirect() {
+    window.location.replace("https://youtu.be/DLzxrzFCyOs");
+}
+
+window.onload = today => {isTodayFriday(today)};
+window.onresize = () => {if (window.innerWidth > 640 ) document.getElementsByClassName('nav-list')[0].classList.remove('responsive')};
