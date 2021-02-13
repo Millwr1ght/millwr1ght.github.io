0540347da918b8c567c872bd50c2fee8d6655185
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-12 21:18:43 -0800

    fix the formula

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -19,10 +19,11 @@ let windChillOutput = document.getElementById('summary-wind-chill');
 //get inputs
 let temperature = parseFloat(document.getElementById('summary-temp').value);
 let windSpeed = parseFloat(document.getElementById('summary-wind-speed').value);
+console.log(temperature,temperature <= 50, windSpeed, windspeed > 3)
 //check inputs
-if (temperature > 50 || windSpeed <= 3.0)  {
+if (temperature > 50 || windSpeed <= 3)  {
     windChillOutput.innerHTML = "N/A";
 } else {
-    let windChill = 35.74 + 0.6215*temperature - 35.75*windSpeed^(0.16) + 0.4275*temperature*windSpeed^(0.16);
+    let windChill = 35.74 + (0.6215*temperature) - (35.75*Math.pow(windSpeed, 0.16)) + (0.4275*temperature*Math.pow(windSpeed, 0.16));
     windChillOutput.innerHTML = windChill + "°F";
 }
