8f2b094996b9024e5153008ddfe048ad62156dcf
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-18 22:58:48 -0800

    Update lazyload.js

--- lesson-7/js/lazyload.js
+++ lesson-7/js/lazyload.js
@@ -2,20 +2,26 @@ const images = document.querySelectorAll("img[data-src]");
 
 function preloadImage(img) {
     img.setAttribute('src', img.getAttribute('data-src'));
-    img.onload = () => {img.removeAttribute('data-src');};
+    img.onload = () => { img.removeAttribute('data-src'); };
 }
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        });
     });
-});
 
-images.forEach(image => {
-    imgObserver(image);
-})
+    images.forEach((image) => {
+        imgObserver.observe(image);
+    }) 
+} else {
+    images.forEach((img) => {
+        loadImages(img);
+    });
+}
