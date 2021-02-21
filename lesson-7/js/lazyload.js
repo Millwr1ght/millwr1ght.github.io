ad0bb109dfeeae2fdc83af1131122e97cf4f2e9d
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-20 18:18:44 -0800

    forgot the lazy code

---
+++ lesson-7/js/lazyload.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => { img.removeAttribute('data-src'); };
+}
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        });
+    });
+
+    images.forEach((image) => {
+        imgObserver.observe(image);
+    }) 
+} else {
+    images.forEach((img) => {
+        loadImages(img);
+    });
+}
