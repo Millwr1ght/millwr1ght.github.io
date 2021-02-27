1aa42f8afa30c1edf2d174da759f80cafbe7ce88
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-02-27 01:24:11 -0800

    dasfs
    adsfghjkmnbghjkm,mnbnm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let d = new Date();
 console.log(d, d.getFullYear());
-document.getElementById('thisYear').textContent = d.getFullYear();
+document.getElementById('thisYear').innerHTML = d.getFullYear();
 
 let lastModified = document.lastModified;
 console.log(document.lastModified, lastModified);
