eacb2b29d4f54bef6d03d0c1457ab2b8cd22f695
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-04 23:02:06 -0800

    Update prophets.js

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -11,28 +11,33 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
+    console.log(prophets);
     //loop over each element in the "meat" of the response
-    prophets.forEach(function (prophet) {
+    prophets.forEach(function(prophet) {
       //build the HTML of a prophet card using the createElement(), textContent(), and appendChild() methods
       let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let birthDate = document.createElement('p');
-      let birthPlace = document.createElement('p');
-      let image = document.createElement('img');
-
+      
+      
+      
+     
+      
       //title w/name
+      let h2 = document.createElement('h2');
       h2.textContent = prophets[prophet].name + ' ' + prophets[prophet].lastname;
       card.appendChild(h2);
 
       //birth date
+      let birthDate = document.createElement('p');
       birthDate.textContent = "Date of birth: " + prophets[prophet].birthdate;
       card.appendChild(birthDate);
 
       //birth place
+      let birthPlace = document.createElement('p');
       birthPlace.textContent = "Place of birth: " + prophets[prophet].birthplace;
       card.appendChild(birthPlace);
 
-      //image
+      //image 
+      let image = document.createElement('img');
       image.setAttribute('src', prophets[prophet].imageurl).setAttribute('alt', prophets[prophet].name + prophets[prophet].lastname + ' - ' + prophets[prophet].order);
       card.appendChild(image);
 
