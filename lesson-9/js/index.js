29dee15f44edc030ce7ddfb0280bcce9ab5a9744
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-06 20:47:17 -0800

    mnijbuvyct
    "/][;.,lpmojihugy

--- lesson-9/js/index.js
+++ lesson-9/js/index.js
@@ -14,3 +14,56 @@ const topNavList = document.getElementById('top-nav-list');
 
 navButton.addEventListener('click', () => {topNavList.classList.toggle('responsive')}, false);
 window.onresize = () => {if (window.innerWidth > 640) topNavList.classList.remove('responsive')};
+
+
+//populate town-cards
+
+const requestURL = '';
+const KWDDtowns = ['Preston', 'Fish Haven', 'Soda Springs'];
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  //now with that response
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++) {
+        let town = towns[i];
+        if (KWDDtowns.includes(town)) {
+            //create town card
+            let townCard = document.createElement('section');
+            townCard.setAttribute('id', town.name);
+
+            //town name
+            let h2 = document.createElement('h2');
+            h2.textContent = town.name;
+            townCard.appendChild(h2);
+
+            //town motto,
+            let motto = document.createElement('p');
+            motto.textContent = town.motto;
+            townCard.appendChild(motto);
+
+            //town year founded,
+            let founded = document.createElement('p');
+            founded.textContent = "Founded in: " + town.yearFounded;
+            townCard.appendChild(founded);
+            
+            //town population,
+            let population = document.createElement('p');
+            population.textContent = "Population: " + town.currentPopulation;
+            townCard.appendChild(population);
+            
+            //town annual rainfall
+            let rainfall = document.createElement('p');
+            rainfall.textContent = "Average rainfall (inches): " + town.averageRainfall;
+            townCard.appendChild(rainfall);
+
+            //add to the DOM
+            document.getElementById('cards').appendChild(card);
+        
+        }
+    };
+});
