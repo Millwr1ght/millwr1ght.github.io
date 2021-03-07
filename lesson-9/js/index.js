5e96a0cb3bbe5300745f3f6a223e333ba6fd558a
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-06 22:33:01 -0800

    Update index.js

--- lesson-9/js/index.js
+++ lesson-9/js/index.js
@@ -70,7 +70,7 @@ fetch(requestURL)
             townPicture.setAttribute('src', `images/${town.photo}`)
 
             //add to the DOM
-            document.getElementById('town-cards').appendChild(townCard);
+            document.getElementById('town-cards').appendChild(townC=ard);
         
         }
     };
