6f37ea4d0db6614977dcdc74af92f9a103986018
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-06 17:26:20 -0800

    that i would do my work before thursday

---
+++ lesson-9/js/index.js
@@ -0,0 +1,16 @@
+let lastModified = document.lastModified;
+const updateSpan = document.getElementById('lastUpdated');
+updateSpan.innerHTML = lastModified;
+
+let d = new Date();
+let today = d.getDay();
+let year = d.getFullYear()
+const yearSpan = document.getElementById('thisYear');
+yearSpan.innerHTML = year;
+
+//nav
+const navButton = document.getElementById('nav-button');
+const topNavList = document.getElementById('top-nav-list');
+
+navButton.addEventListener('click', () => {topNavList.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 640) topNavList.classList.remove('responsive')};
