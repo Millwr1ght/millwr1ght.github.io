31cf33843ef4489c4c5379243c7d8d411fad65fe
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-10 15:55:45 -0800

    so much

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,9 +1,7 @@
 //declare
 const cityID = "5604473";
 const apiKey = "f7e1c17f0ec2e5c6172315506ce89862";
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}&units=imperial`
-
-
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}&units=imperial` 
 
 //fetch
 fetch(apiURL)
@@ -18,5 +16,12 @@ fetch(apiURL)
         document.getElementById('icon').setAttribute('alt', desc);
 
         //get current temp
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('current-temp').textContent = jsObject.main.temp; //testing
+
+        //fill the weather summary box
+        document.getElementById('summary-condition').textContent = jsObject.main.main; //current condition
+        document.getElementById('summary-temp').textContent = jsObject.main.temp; //current temp
+        //generate elsewhere // document.getElementByID('summary-wind-chill').textContent = jsObject.main.feels_like; //wind chill
+        document.getElementById('summary-humid').textContent = jsObject.main.humidity; //current humid
+        document.getElementById('summary-speed').textContent = jsObject.wind.speed; //wind speed
     });
