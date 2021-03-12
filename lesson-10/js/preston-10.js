b04da9ab249d2a7d53fb5b515b35b0647a1639f6
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-11 16:12:46 -0800

    Create preston-10.js

---
+++ lesson-10/js/preston-10.js
@@ -0,0 +1,18 @@
+let lastModified = document.lastModified;
+const updateSpan = document.getElementById('lastUpdated');
+updateSpan.innerHTML = lastModified;
+
+let d = new Date();
+let today = d.getDay();
+let year = d.getFullYear()
+const yearSpan = document.getElementById('thisYear');
+yearSpan.innerHTML = year;
+
+//nav
+//const navButton = document.getElementById('nav-button');
+
+function navToggle() {
+    let topNavList = document.getElementById('nav-list');
+    topNavList.classList.toggle('responsive');
+};
+window.onresize = () => {if (window.innerWidth > 640) topNavList.classList.remove('responsive')};
