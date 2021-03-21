7540560923f686c634c9e68a7050a2e644e23b86
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-03-20 20:48:50 -0700

    gdfhgfjhgk

--- lesson-11/js/weatherapi.js
+++ lesson-11/js/weatherapi.js
@@ -25,7 +25,7 @@ fetch(apiURL_current)
         let windSpeed = jsObject.wind.speed;
         console.log(temperature, temperature <= 50, windSpeed, windSpeed > 3) //check inputs
         if (temperature > 50 || windSpeed <= 3) {
-            windChillOutput.innerHTML = "N/A";
+            document.getElementById('summary-wind-chill').innerHTML = "N/A";
         } else {
             let windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
             document.getElementById('summary-wind-chill').innerHTML = Math.round(windChill) + "°F";
