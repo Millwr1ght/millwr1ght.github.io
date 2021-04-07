d84fed6a52e22ad50b7ddb69340091ce07d0f36f
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-04-07 08:50:27 -0700

    aiyah

---
+++ beryl/js/confirmation.js
@@ -0,0 +1,4 @@
+function confirm() {
+    let output = document.getElementById('confirmation');
+    output.innerText = 'Thank you for signing up for our newsletter!';
+}
--- beryl/js/directory.js
+++ beryl/js/directory.js
@@ -14,25 +14,35 @@ fetch(directoryURL)
             //Business name
             let h2 = document.createElement('h2');
             h2.textContent = cardContent.name;
+            h2.classList.add('card-title');
+            card.appendChild(h2);
 
             //Physical address
             let address = document.createElement('p');
             address.textContent = cardContent.address;
+            address.classList.add('card-address');
+            card.appendChild(address);
 
             //Phone Number
             let phone = document.createElement('p');
             phone.textContent = cardContent.phone;
+            phone.classList.add('card-number');
+            card.appendChild(phone);
 
             //website, if they have one
             if (cardContent.website != "") {
                 let website = document.createElement('p');
                 website.textContent = cardContent.website;
+                website.classList.add('card-site');
+                card.appendChild(website);
             }
 
             //email address, if they have one
             if (cardContent.email != "") {
                 let email = document.createElement('p');
                 email.textContent = cardContent.email;
+                card.appendChild(email);
+                email.classList.add('card-email');
             }
 
             //logo, if there is one
@@ -40,7 +50,14 @@ fetch(directoryURL)
                 let logo = document.createElement('img');
                 logo.setAttribute('src', cardContent.logo);
                 logo.setAttribute('alt', cardContent.name);
+                logo.classList.add('card-img');
+                card.appendChild(logo);
             }
+
+            
+            
+            
+            
 
 
 
